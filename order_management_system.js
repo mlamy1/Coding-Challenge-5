64010608dc757dd0f55c1d8c0f477be97d93c0fb
Myia Lamy <mlamy@usf.edu>
2024-09-20 01:51:04 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,10 @@
+// Task 1: Create an Inventory Array of Product Objects
+
+const inventory = [
+    {name: 'Chocolate Chip', price: 3, quantity: 20},
+    {name: 'Double Chocolate Chip', price: 4, quantity: 15},
+    {name: 'Sugar', price: 3, quantity: 20},
+    {name: 'Birthday Cake', price: 4, quantity: 15} 
+]; 
+// Array contains name, price, and quantity of products. 
+
